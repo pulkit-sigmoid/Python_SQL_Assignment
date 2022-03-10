f207cccde9ea48bd028a9df9dd7185d2c186a67d
pulkit-sigmoid <98748483+pulkit-sigmoid@users.noreply.github.com>
2022-03-10 17:08:51 +0530

    Update ques3.py

--- ques3.py
+++ ques3.py
@@ -1,7 +1,7 @@
 import logging
 
-import pandas
 import psycopg2.extras
+import xlsxwriter
 
 logging.basicConfig(filename="log.txt",
                     level=logging.INFO,
@@ -9,6 +9,7 @@ logging.basicConfig(filename="log.txt",
 
 conn = None
 cur = None
+wb3 = None
 try:
 
     conn = psycopg2.connect(
@@ -22,21 +23,37 @@ try:
 
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    # Question 3 Read and upload the above xlsx into a new table in the Postgres DB (Sigmoid)
+    # Question 4 From the xlsx in 2) create another xlsx to list total compensation given at Department level till date.
+    # Columns: Dept No, Dept,Name, Compensation
 
-    df = pandas.read_excel('/Users/pulkitgupta/code/PycharmProjects/pythonProject/Python_SQL_Assignment/ques2.xlsx')
-    cur.execute("create table if not exists total_compensation ("
-                "empno NUMERIC(4),"
-                "ename VARCHAR(10),"
-                "dname VARCHAR(14),"
-                "compensation NUMERIC(8),"
-                "months NUMERIC(2));")
-    logging.info("\nTotal Compensation Table Created\n")
-    insert_query = "Insert into total_compensation (empno, ename, dname, compensation, months) values (%s,%s,%s,%s,%s)"
-    for index, row in df.iterrows():
-        cur.execute(insert_query, (
-            row['Employee Number'], row['Employee Name'], row['Department Name'], row['Compensation'],
-            row['Total Months Spent']))
+    logging.info("\nTotal Compensation by Department till date\n")
+    cur.execute("select dept.deptno as department_number, dept.dname as department_name, sum(com.compensation) "
+                "as compensation from total_compensation as com "
+                "join dept "
+                "on "
+                "com.dname=dept.dname "
+                "group by dept.deptno;")
+    data = cur.fetchall()
+
+    logging.info("deptno dname compensation")
+    for record in data:
+        logging.info(f"{record['department_number']} "
+                     f"{record['department_name']} "
+                     f"{record['compensation']}")
+
+    wb3 = xlsxwriter.Workbook('/Users/pulkitgupta/code/PycharmProjects/pythonProject/Python_SQL_Assignment/ques3.xlsx')
+    worksheet = wb3.add_worksheet()
+    worksheet.write('A1', 'Department Number')
+    worksheet.write('B1', 'Department Name')
+    worksheet.write('C1', 'Compensation')
+
+    row = 1
+    col = 0
+    for number, name, comp in data:
+        worksheet.write(row, col, number)
+        worksheet.write(row, col + 1, name)
+        worksheet.write(row, col + 2, comp)
+        row += 1
 
     conn.commit()
 
@@ -50,3 +67,5 @@ finally:
         cur.close()
     if conn is not None:
         conn.close()
+    if wb3 is not None:
+        wb3.close()
